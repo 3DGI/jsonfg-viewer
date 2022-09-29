33c37e0738f73a19440a3924fb4bdde92bdb9fab
Ravi Peters <r.y.peters@tudelft.nl>
2022-09-29 19:07:16 +0200

    fix wrong EPSG:7415 definition

--- src/jsonfg-threejs-loader/base/JSONFGLoader.js
+++ src/jsonfg-threejs-loader/base/JSONFGLoader.js
@@ -20,11 +20,8 @@ export class JSONFGLoader {
 
 		// Load proj4 defs
 		proj4.defs(defs);
-		proj4.defs("EPSG:7415","+proj=sterea +lat_0=52.1561605555556 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +vunits=m +no_defs +type=crs");
-		// let p = proj4("EPSG:4326", [
-		// 	4.364435966292724,
-		// 	52.00539642100053,
-		// 	0])
+		proj4.defs("EPSG:7415","+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs +no_defs");
+
 		this.targetCRS = "EPSG:4326";
 
 	}
