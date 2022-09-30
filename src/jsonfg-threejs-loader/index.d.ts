4d63a5e47113e87c092a20261820ecfe56fa03d5
Ylannl <r.y.peters@tudelft.nl>
2022-09-30 10:27:09 +0200

    remove unused imports

--- src/jsonfg-threejs-loader/index.d.ts
+++ src/jsonfg-threejs-loader/index.d.ts
@@ -3,15 +3,14 @@ import { CityJSONWorkerParser } from './parsers/CityJSONWorkerParser';
 import { ChunkParser } from './parsers/helpers/ChunkParser';
 
 import { CityObjectsMesh } from './objects/CityObjectsMesh';
-import { CityObjectsInstancedMesh } from './objects/CityObjectsInstancedMesh';
-import { CityObjectsLines } from './objects/CityObjectsLines';
-import { CityObjectsPoints } from './objects/CityObjectsPoints';
+// import { CityObjectsLines } from './objects/CityObjectsLines';
+// import { CityObjectsPoints } from './objects/CityObjectsPoints';
 
 import { CityObjectsMaterial } from './materials/CityObjectsMaterial';
-import { CityObjectsLineMaterial } from './materials/CityObjectsLineMaterial';
-import { CityObjectsPointsMaterial } from './materials/CityObjectsPointsMaterial';
+// import { CityObjectsLineMaterial } from './materials/CityObjectsLineMaterial';
+// import { CityObjectsPointsMaterial } from './materials/CityObjectsPointsMaterial';
 
-import { AttributeEvaluator } from './helpers/AttributeEvaluator';
+// import { AttributeEvaluator } from './helpers/AttributeEvaluator';
 // import { TextureManager } from './helpers/TextureManager';
 
 export {
@@ -19,15 +18,14 @@ export {
     CityJSONWorkerParser,
     ChunkParser,
 
-    CityObjectsInstancedMesh,
     CityObjectsMesh,
-    CityObjectsLines,
-    CityObjectsPoints,
+    // CityObjectsLines,
+    // CityObjectsPoints,
 
     CityObjectsMaterial,
-    CityObjectsLineMaterial,
-    CityObjectsPointsMaterial,
+    // CityObjectsLineMaterial,
+    // CityObjectsPointsMaterial,
 
-    AttributeEvaluator,
+    // AttributeEvaluator,
     // TextureManager
 };
--- src/jsonfg-threejs-loader/index.js
+++ src/jsonfg-threejs-loader/index.js
@@ -3,15 +3,14 @@ import { CityJSONWorkerParser } from './parsers/CityJSONWorkerParser.js';
 import { ChunkParser } from './parsers/helpers/ChunkParser.js';
 
 import { CityObjectsMesh } from './objects/CityObjectsMesh.js';
-import { CityObjectsInstancedMesh } from './objects/CityObjectsInstancedMesh.js';
-import { CityObjectsLines } from './objects/CityObjectsLines.js';
-import { CityObjectsPoints } from './objects/CityObjectsPoints.js';
+// import { CityObjectsLines } from './objects/CityObjectsLines.js';
+// import { CityObjectsPoints } from './objects/CityObjectsPoints.js';
 
 import { CityObjectsMaterial } from './materials/CityObjectsMaterial.js';
-import { CityObjectsLineMaterial } from './materials/CityObjectsLineMaterial.js';
-import { CityObjectsPointsMaterial } from './materials/CityObjectsPointsMaterial.js';
+// import { CityObjectsLineMaterial } from './materials/CityObjectsLineMaterial.js';
+// import { CityObjectsPointsMaterial } from './materials/CityObjectsPointsMaterial.js';
 
-import { AttributeEvaluator } from './helpers/AttributeEvaluator.js';
+// import { AttributeEvaluator } from './helpers/AttributeEvaluator.js';
 // import { TextureManager } from './helpers/TextureManager.js';
 
 export {
@@ -19,15 +18,14 @@ export {
 	CityJSONWorkerParser,
 	ChunkParser,
 
-	CityObjectsInstancedMesh,
 	CityObjectsMesh,
-	CityObjectsLines,
-	CityObjectsPoints,
+	// CityObjectsLines,
+	// CityObjectsPoints,
 
 	CityObjectsMaterial,
-	CityObjectsLineMaterial,
-	CityObjectsPointsMaterial,
+	// CityObjectsLineMaterial,
+	// CityObjectsPointsMaterial,
 
-	AttributeEvaluator,
+	// AttributeEvaluator,
 	// TextureManager
 };
